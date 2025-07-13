760c24375525750a14dc1761843c25e164593e8a
eyeseeyougood <97800461+eyeseeyougood@users.noreply.github.com>
2025-07-13 17:20:28 +0100

    another UI fix
    - fixed VerticalScrollView anchor maths (hopefully)
    - fixed forced rendering for out of bounds elements in VerticalScrollView

--- Bearing/Bearing Engine/UIElements.cs
+++ Bearing/Bearing Engine/UIElements.cs
@@ -473,8 +473,10 @@ public class UIVerticalScrollView : UIElement
 
             float elementOffset = newIndex * elementNormalisedScale.Y * Game.instance.ClientSize.Y;
 
-            element.position = new UDim2(position.scale - new Vector2(0, 0.5f * normalisedScale.Y - elementNormalisedScale.Y * (1-element.anchor.Y)), new Vector2(0, newIndex*spacing + elementOffset));
+            element.position = new UDim2(position.scale, new Vector2(0, newIndex*spacing + elementOffset));
             element.size = new UDim2(new Vector2(size.scale.X, element.size.scale.Y), element.size.offset + new Vector2(size.offset.X, 0));
+
+            element.anchor = anchor;
 
             // check if still in bounding box otherwise dont render
 
@@ -488,7 +490,6 @@ public class UIVerticalScrollView : UIElement
             else if (!Extensions.PointInQuad(ebb.Zw, obb))
                 shouldRender = false;
 
-            shouldRender = true;
             element.visible = shouldRender;
 
             index++;
